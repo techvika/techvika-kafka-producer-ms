5b9e98c140562ebf868e6fcaa46bd338be7ab82c
Mahesh <techvika.official@gmail.com>
2026-07-21 10:38:51 +0530

    initial commit

---
+++ src/main/java/com/techvika/producer/TechvikaKafkaProducerMsApplication.java
@@ -0,0 +1,13 @@
+package com.techvika.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechvikaKafkaProducerMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechvikaKafkaProducerMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techvika/producer/TechvikaKafkaProducerMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techvika.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechvikaKafkaProducerMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
